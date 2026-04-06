4f3ecc2b0ff20ed20b5dc97dca30d43a76727911
patutos1 <129093150+patutos1@users.noreply.github.com>
2026-04-06 16:47:52 -0400

    Removed the excess "Add Note" button

--- script.js
+++ script.js
@@ -1,62 +1,54 @@
-//Note Script 
-const NOTES_KEY = "notes";
-const noteContainer = document.querySelector(".note-container");
+document.addEventListener("DOMContentLoaded", () => {
 
-function getNotes() {
-	if (!localStorage.getItem(NOTES_KEY))
-		localStorage.setItem(NOTES_KEY, JSON.stringify([]));
-	return JSON.parse(localStorage.getItem(NOTES_KEY));
+	const NOTES_KEY = "notes";
+	const noteContainer = document.querySelector(".note-container");
+
+	function getNotes() {
+		if (!localStorage.getItem(NOTES_KEY))
+			localStorage.setItem(NOTES_KEY, JSON.stringify([]));
+		return JSON.parse(localStorage.getItem(NOTES_KEY));
 	}
 
-function setNotes(notes) {
-	localStorage.setItem(NOTES_KEY, JSON.stringify(notes));
-}
+	function setNotes(notes) {
+		localStorage.setItem(NOTES_KEY, JSON.stringify(notes));
+	}
 	
-function collectNotes() {
-	var notes = [];
-	document.querySelectorAll(".note textarea").forEach(note => {
-		if (note.value == "") {
-			note.parentElement.remove()
-		} else {
-			notes.push(note.value);
-		}
+	function collectNotes() {
+		var notes = [];
+		document.querySelectorAll(".note textarea").forEach(note => {
+			if (note.value == "") {
+				note.parentElement.remove()
+			} else {
+				notes.push(note.value);
+			}
+		});
 		setNotes(notes);
-	})
-}
-
-function cleanNote() {
-	var note = document.activeElement;
-	if (note.value == "(new note)") {
-	    note.value = "";
 	}
-}
+
+	function cleanNote() {
+		var note = document.activeElement;
+		if (note.value == "(new note)") {
+			note.value = "";
+		}
+	}
 	
-function addNote() {
-	document.getElementById("addbutton").remove();
-	appendNote("(new note)");
-	appendNoteButton();
-	collectNotes();
-}
-function appendNote(text) {
-	var note = document.createElement("div");
-	note.classList.add("note");
-	note.innerHTML = `<textarea>${text}</textarea>`;
-	note.addEventListener("focusin", cleanNote);
-	note.addEventListener("focusout", collectNotes);
-	noteContainer.appendChild(note);
-}
+	function addNote() {
+		appendNote("(new note)");
+		collectNotes();
+	}
 
-function appendNoteButton() {
-	var addButton = document.createElement("button");
-	addButton.id = "addbutton";
-	addButton.innerHTML = '+ Add Note';
-	addButton.addEventListener("click", addNote);
-	noteContainer.appendChild(addButton);
-}
+	function appendNote(text) {
+		var note = document.createElement("div");
+		note.classList.add("note");
+		note.innerHTML = `<textarea>${text}</textarea>`;
+		note.addEventListener("focusin", cleanNote);
+		note.addEventListener("focusout", collectNotes);
+		noteContainer.appendChild(note);
+	}
 
-var notes = getNotes();
+	var notes = getNotes();
+	notes.forEach(n => appendNote(n));
 
-notes.forEach(n => {appendNote(n)});
-appendNoteButton();
+	document.getElementById("addNoteIcon")?.addEventListener("click", addNote);
 
-document.getElementById("addNoteIcon")?.addEventListener("click", addNote);
+});
